ff6f6a5e68228730b5a219871ee34a270107958b
“Piyush <piyushrawal93@gmail.com>
2024-12-29 12:54:48 +0530

    csv_file_modified

--- corona_project/corona_data.py
+++ corona_project/corona_data.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import numpy as np # import numpy as np
-corona_data = pd.read_csv('\project_1\complete.csv')
+corona_data = pd.read_csv('piyush1975\project_1\complete.csv')
 corona_data()
